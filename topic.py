74acb0dc5b703f19b0eb76731103893cac354354
Elard Guevara <elard.guevara@ucsp.edu.pe>
2016-06-30 11:08:41 -0500

    subiendo

--- topic.py
+++ topic.py
@@ -1,32 +1,48 @@
 import sys
 from ngram import NGram
 
-class TopicDetector:
+class TopicDetector: 
 	def __init__(self,text):
 		self.ng=[]
 		self.topic=[]
 		file = open(text,"r")
-		
 		for linea in file.readlines():
-    		words = linea.split("\t")
-    		topic+=[words[0]]
-    		nng=NGram(words[1].split(" "))
-    		ng+=[nng]
-    	
+			words = linea.split(" ")
+    		self.topic += [words[0]]
+    		nng = NGram(words[2].split(","))
+    		self.ng += [nng]
     	file.close()
+
+    	'''file = open(text,"r")
+		linea = file.readline()
+		while linea != '':
+			words = linea.split(" ")
+    		self.topic += [words[0]]
+    		nng = NGram(words[2].split(","))
+    		self.ng += [nng]
+			linea = file.readline()
+		file.close()'''
 
 	def verify(self,text_compare):
 		results = []
-		file = open(text_compare,"r")
-		
 		texto = []
-		for linea in file.readlines():
-			texto+=linea.split(" ")
+
+		'''file2 = open(text_compare,"r")
+		for linea2 in file2.readlines():
+			texto+=linea2.split(" ")
 		tng=NGram(texto)
+		file2.close()'''
 
-		file.close()
+		file2 = open(text,"r")
+		linea2 = file2.readline()
+		while linea2 != '':
+			if linea2 != '\n':
+				texto+=linea2.split(" ")
+			linea = file.readline()
+		tng=NGram(texto)
+		file2.close()
 
-		for ngs in ng:
+		for ngs in self.ng:
 			count=0
 			for word in list(ng):
 				count+=len(tng.items_sharing_ngrams(word))
@@ -41,7 +57,7 @@ class TopicDetector:
 				pos=i
 			i+=1
 
-		print("Tema mas preciso del texto: "+repr(topic[pos]))
+		print("Tema mas preciso del texto: "+repr(self.topic[pos]))
 				
 
 			
